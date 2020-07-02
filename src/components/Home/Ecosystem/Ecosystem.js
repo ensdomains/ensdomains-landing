cfeb6d35ee4c548c430fb6782fdc6f2271f2d1a7
Jeff Lau <jeff-lau@live.com>
2020-07-02 15:03:41 +0800

    add sorting for featured

--- src/components/Home/Ecosystem/Ecosystem.js
+++ src/components/Home/Ecosystem/Ecosystem.js
@@ -5,14 +5,60 @@ import { motion } from "framer-motion"
 import { H2 } from "../../Typography"
 import { importAll } from "../../../utils"
 import links from "../links.json"
+import appSvg from "./app.svg"
+import browserSvg from "./browser.svg"
+import walletSvg from "./wallet.svg"
 
-const wallets = importAll(
-  require.context("./wallets", false, /\.(png|jpe?g|svg)$/)
+import mq from "../../../mediaQuery"
+
+function sortApps(array, property) {
+  return array.sort((a, b) => {
+    if (a[property]) {
+      return -1
+    }
+
+    if (a[property] && b[property]) {
+      return a[property] < b[property] ? -1 : 1
+    }
+
+    return a.name < b.name
+  })
+}
+
+const wallets = sortApps(
+  importAll(require.context("./wallets", false, /\.(png|jpe?g|svg)$/)).map(
+    item => {
+      return {
+        ...item,
+        ...links[item.fileName],
+      }
+    }
+  ),
+  "featuredWallet"
 )
-const apps = importAll(require.context("./apps", false, /\.(png|jpe?g|svg)$/))
 
-const browsers = importAll(
-  require.context("./browsers", false, /\.(png|jpe?g|svg)$/)
+const apps = sortApps(
+  importAll(require.context("./apps", false, /\.(png|jpe?g|svg)$/)).map(
+    item => {
+      return {
+        ...item,
+        ...links[item.fileName],
+      }
+    }
+  ),
+  "featuredApp"
+)
+
+const browsers = sortApps(
+  importAll(require.context("./browsers", false, /\.(png|jpe?g|svg)$/)).map(
+    item => {
+      return {
+        ...item,
+        ...links[item.fileName],
+      }
+    }
+  ),
+  "featuredBrowser"
 )
 
 const HeroContainer = styled("div")`
@@ -26,6 +72,22 @@ const Section = styled("section")`
   display: flex;
   flex-direction: column;
   align-items: center;
+  max-width: 800px;
+  width: 100%;
+  padding: 20px 40px;
+  margin-bottom: 30px;
+  border-radius: 20px;
+  box-shadow: 2px 8px 25px 2px rgba(136, 149, 169, 0.12);
+
+  > h3 {
+    > img {
+      margin-right: 8px;
+    }
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+    font-size: 22px;
+  }
 `
 
 const Grid = styled(motion.div)`
@@ -33,7 +95,6 @@ const Grid = styled(motion.div)`
   overflow: hidden;
   grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
   grid-gap: 50px;
-  max-width: 800px;
   width: 100%;
 `
 
@@ -59,7 +120,7 @@ const GridItemContainer = styled("a")`
 `
 
 function GridItem({ app }) {
-  const details = links[app.fileName]
+  const details = app
   return (
     <GridItemContainer href={details.link}>
       <img src={app.src} />
@@ -69,6 +130,11 @@ function GridItem({ app }) {
 }
 
 const More = styled("p")`
+  font-size: 20px;
+  color: #2c46a6;
+  letter-spacing: 0;
+  text-align: center;
+
   &:hover {
     cursor: pointer;
   }
@@ -85,36 +151,45 @@ export default function Ecosystem(props) {
         <a href="">Instructions</a> to add your logo
       </p>
       <Section>
-        <h3>Wallets</h3>
+        <h3>
+          <img src={walletSvg} />
+          Wallets
+        </h3>
         <Grid animate={{ height: moreWallets ? "auto" : "200px" }}>
           {wallets.map(app => (
             <GridItem app={app} />
           ))}
         </Grid>
         <More href="#" onClick={() => setMoreWallets(!moreWallets)}>
-          {moreWallets ? "See less" : "See more"}
+          {moreWallets ? "See less - " : "See more +"}
         </More>
       </Section>
       <Section>
-        <h3>Apps</h3>
+        <h3>
+          <img src={appSvg} />
+          Apps
+        </h3>
         <Grid animate={{ height: moreApps ? "auto" : "200px" }}>
           {apps.map(app => (
             <GridItem app={app} />
           ))}
         </Grid>
         <More href="#" onClick={() => setMoreApps(!moreApps)}>
-          {moreApps ? "See less" : "See more"}
+          {moreApps ? "See less - " : "See more +"}
         </More>
       </Section>
       <Section>
-        <h3>Browsers</h3>
+        <h3>
+          <img src={browserSvg} />
+          Browsers
+        </h3>
         <Grid animate={{ height: moreBrowsers ? "auto" : "200px" }}>
           {browsers.map(app => (
             <GridItem app={app} />
           ))}
         </Grid>
         <More href="#" onClick={() => setMoreBrowsers(!moreBrowsers)}>
-          {moreBrowsers ? "See less" : "See more"}
+          {moreBrowsers ? "See less - " : "See more +"}
         </More>
       </Section>
     </HeroContainer>
