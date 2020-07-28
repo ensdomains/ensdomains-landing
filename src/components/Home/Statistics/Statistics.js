18b223b82abb740125ed5232ddd7635a1eea539a
Brantly Millegan <bcmillegan@gmail.com>
2020-07-28 13:02:44 -0700

    Update Statistics.js (#23)

--- src/components/Home/Statistics/Statistics.js
+++ src/components/Home/Statistics/Statistics.js
@@ -83,7 +83,7 @@ export default function Statistics(props) {
       <H2>ENS is the most widely integrated blockchain naming standard.</H2>
       <Stats>
         <p>
-          <strong>445k</strong> <span>registered names</span>
+          <strong>446k</strong> <span>registered names</span>
         </p>
         <p>
           <strong>{integrationsCount}</strong> <span>integrated services</span>
