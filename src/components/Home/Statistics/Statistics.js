c5a844bccf75a9a0c151939fdf5daf144b53fffb
Brantly Millegan <bcmillegan@gmail.com>
2020-10-12 13:38:40 -0600

    Update Statistics.js

--- src/components/Home/Statistics/Statistics.js
+++ src/components/Home/Statistics/Statistics.js
@@ -85,14 +85,14 @@ export default function Statistics(props) {
       <H2>{t("home.statistics.title")}</H2>
       <Stats>
         <p>
-          <strong>194k</strong> <span>{t("home.statistics.names")}</span>
+          <strong>196k</strong> <span>{t("home.statistics.names")}</span>
         </p>
         <p>
           <strong>{integrationsCount}</strong>{" "}
           <span>{t("home.statistics.services")}</span>
         </p>
         <p>
-          <strong>53.8k</strong> <span>{t("home.statistics.owners")}</span>
+          <strong>54.6k</strong> <span>{t("home.statistics.owners")}</span>
         </p>
       </Stats>
     </HeroContainer>
