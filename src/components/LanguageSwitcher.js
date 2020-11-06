a3b6baf59d927fdb52e58aad31653f574ad5afea
Jeff Lau <jeff-lau@live.com>
2020-11-06 12:38:46 +0800

    Add clean script

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -1,11 +1,8 @@
-import { useTranslation } from "react-i18next"
 import { motion, AnimatePresence } from "framer-motion"
 import styled from "@emotion/styled"
 import React, { useState, useContext } from "react"
 import { useTranslation } from "react-i18next"
 
-import { motion, AnimatePresence } from "framer-motion"
-import styled from "@emotion/styled"
 import { Link, useI18next, I18nextContext } from "gatsby-plugin-react-i18next"
 
 import RotatingSmallCaret from "./Icons/RotatingSmallCaret"
