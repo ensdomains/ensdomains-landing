8c33cce49e9558383a68deea675f073b7b8b7f7a
Brantly Millegan <bcmillegan@gmail.com>
2020-11-30 11:20:02 -0700

    Revert "Switch see more and see less on homepage (#91)"
    This reverts commit 66769d9e803371f3aca11ed79f615ad48d9b424b.

--- src/components/Home/Statistics/Statistics.js
+++ src/components/Home/Statistics/Statistics.js
@@ -85,14 +85,14 @@ export default function Statistics(props) {
       <H2>{t("home.statistics.title")}</H2>
       <Stats>
         <p>
-          <strong>205k</strong> <span>{t("home.statistics.names")}</span>
+          <strong>204k</strong> <span>{t("home.statistics.names")}</span>
         </p>
         <p>
           <strong>{integrationsCount}</strong>{" "}
           <span>{t("home.statistics.services")}</span>
         </p>
         <p>
-          <strong>57.7k</strong> <span>{t("home.statistics.owners")}</span>
+          <strong>57.5k</strong> <span>{t("home.statistics.owners")}</span>
         </p>
       </Stats>
     </HeroContainer>
