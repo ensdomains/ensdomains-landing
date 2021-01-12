f7cd49c6530368d16d92c474a7a14ada6b901123
Brantly Millegan <bcmillegan@gmail.com>
2021-01-12 10:57:00 -0700

    Update Statistics.js

--- src/components/Home/Statistics/Statistics.js
+++ src/components/Home/Statistics/Statistics.js
@@ -92,7 +92,7 @@ export default function Statistics(props) {
           <span>{t("home.statistics.services")}</span>
         </p>
         <p>
-          <strong>60.1k</strong> <span>{t("home.statistics.owners")}</span>
+          <strong>60.6k</strong> <span>{t("home.statistics.owners")}</span>
         </p>
       </Stats>
     </HeroContainer>
