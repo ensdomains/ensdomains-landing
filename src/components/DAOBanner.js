7cbdd71d0d2ec236d522327885c2a10ce162808a
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-11-01 20:27:59 +0000

    Fixed the grammatical error

--- src/components/DAOBanner.js
+++ src/components/DAOBanner.js
@@ -68,7 +68,7 @@ export function DAOBannerContent() {
       <div>
         <BannerTitle>ENS DAO: A Call for Delegates</BannerTitle>
         <BannerContent>
-          ENS is decentralizing governance, and are accepting submissions for delegates.
+          ENS is decentralizing governance, and accepting submissions for delegates.
         </BannerContent>
       </div>
       <ArrowLink target="_blank" rel="noreferrer" href="https://ens.mirror.xyz">
