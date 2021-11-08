c89912e5d4a57623b12d00fc4206224fc59eb5f7
brantly.eth <bcmillegan@gmail.com>
2021-11-08 12:25:07 -0500

    Add logo update stats (#318)
    * Update links.json
    
    * Add files via upload
    
    * Update Statistics.js

--- src/components/Home/Statistics/Statistics.js
+++ src/components/Home/Statistics/Statistics.js
@@ -88,14 +88,14 @@ export default function Statistics(props) {
       </AnchorContainer>
       <Stats>
         <p>
-          <strong>410k</strong> <span>{t("home.statistics.names")}</span>
+          <strong>411k</strong> <span>{t("home.statistics.names")}</span>
         </p>
         <p>
           <strong>{integrationsCount}</strong>{" "}
           <span>{t("home.statistics.services")}</span>
         </p>
         <p>
-          <strong>156k</strong> <span>{t("home.statistics.owners")}</span>
+          <strong>157k</strong> <span>{t("home.statistics.owners")}</span>
         </p>
       </Stats>
     </HeroContainer>
