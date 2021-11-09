8057cad4468d5713c382f25db4e23f26dee6e6a8
brantly.eth <bcmillegan@gmail.com>
2021-11-09 14:31:01 -0500

    Update becca photo (#322)
    * Delete becca.png
    
    * Add files via upload
    
    * Update Statistics.js

--- src/components/Home/Statistics/Statistics.js
+++ src/components/Home/Statistics/Statistics.js
@@ -88,14 +88,14 @@ export default function Statistics(props) {
       </AnchorContainer>
       <Stats>
         <p>
-          <strong>411k</strong> <span>{t("home.statistics.names")}</span>
+          <strong>416k</strong> <span>{t("home.statistics.names")}</span>
         </p>
         <p>
           <strong>{integrationsCount}</strong>{" "}
           <span>{t("home.statistics.services")}</span>
         </p>
         <p>
-          <strong>157k</strong> <span>{t("home.statistics.owners")}</span>
+          <strong>160k</strong> <span>{t("home.statistics.owners")}</span>
         </p>
       </Stats>
     </HeroContainer>
