e4602c9068bac8f0b9055b78d8a11c2491700a69
Leon Talbert <leon.talbert@gmail.com>
2021-11-18 12:46:28 +0800

    add missing keys

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,44 +76,6 @@ module.exports = {
       },
     },
     {
-      // including a plugin from outside the plugins folder needs the path to it
-      resolve: `gatsby-plugin-react-i18next`,
-      options: {
-        languages: ["en"],
-        defaultLanguage: "en",
-        path: `${__dirname}/locales`,
-        siteUrl: "app.ens.domains",
-        i18nextOptions: {
-          debug: true,
-          lng: "en",
-          fallbackLng: "en",
-          whitelist: [
-            "cn",
-            "de",
-            "en",
-            "es",
-            "fr",
-            "ko",
-            "it",
-            "ja",
-            "nl",
-            "pl",
-            "ru",
-            "vi",
-          ],
-          interpolation: {
-            escapeValue: false, // not needed for react as it escapes by default
-          },
-        },
-        pages: [
-          {
-            matchPath: "/ignored-page",
-            languages: ["en"],
-          },
-        ],
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-101611202-2",
