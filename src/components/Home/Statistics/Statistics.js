a245d6d91b0deb2adf37df02f85e06ba16643520
Bigint <bigint@icloud.com>
2022-01-07 19:13:25 +0530

    chore: update stats

--- src/components/Home/Statistics/Statistics.js
+++ src/components/Home/Statistics/Statistics.js
@@ -88,14 +88,14 @@ export default function Statistics(props) {
       </AnchorContainer>
       <Stats>
         <p>
-          <strong>618k</strong> <span>{t("home.statistics.names")}</span>
+          <strong>620k</strong> <span>{t("home.statistics.names")}</span>
         </p>
         <p>
           <strong>{integrationsCount}</strong>{" "}
           <span>{t("home.statistics.services")}</span>
         </p>
         <p>
-          <strong>262k</strong> <span>{t("home.statistics.owners")}</span>
+          <strong>263k</strong> <span>{t("home.statistics.owners")}</span>
         </p>
       </Stats>
     </HeroContainer>
