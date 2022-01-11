1a6ec01f564ef4e04884d95ebd914d9d8be9ff2e
bigint <bigint@icloud.com>
2022-01-11 22:15:53 +0530

    chore: update stats and add logos (#414)
    * chore: add song a day
    
    * chore: add Eth.army
    
    * chore: update stats
    
    * feat: add wepiggy
    
    * feat: add Beefy.finance
    
    * feat: add omnibridge
    
    * chore: update stats
    
    * feat: add 0exchange
    
    * feat: add squeeth
    
    * chore: update stats
    
    * chore: add bombmoney

--- src/components/Home/Statistics/Statistics.js
+++ src/components/Home/Statistics/Statistics.js
@@ -91,14 +91,14 @@ export default function Statistics(props) {
       </AnchorContainer>
       <Stats>
         <p>
-          <strong>628k</strong> <span>{t("home.statistics.names")}</span>
+          <strong>630k</strong> <span>{t("home.statistics.names")}</span>
         </p>
         <p>
           <strong>{integrationsCount}</strong>{" "}
           <span>{t("home.statistics.services")}</span>
         </p>
         <p>
-          <strong>267k</strong> <span>{t("home.statistics.owners")}</span>
+          <strong>268k</strong> <span>{t("home.statistics.owners")}</span>
         </p>
       </Stats>
     </HeroContainer>
