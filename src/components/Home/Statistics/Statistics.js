5c1a3ce49091bd40cb25439243d29d6543d52622
Bigint <bigint@icloud.com>
2022-01-12 08:54:38 +0530

    chore: update stats and add logos

--- src/components/Home/Statistics/Statistics.js
+++ src/components/Home/Statistics/Statistics.js
@@ -91,14 +91,14 @@ export default function Statistics(props) {
       </AnchorContainer>
       <Stats>
         <p>
-          <strong>630k</strong> <span>{t("home.statistics.names")}</span>
+          <strong>631k</strong> <span>{t("home.statistics.names")}</span>
         </p>
         <p>
           <strong>{integrationsCount}</strong>{" "}
           <span>{t("home.statistics.services")}</span>
         </p>
         <p>
-          <strong>268k</strong> <span>{t("home.statistics.owners")}</span>
+          <strong>269k</strong> <span>{t("home.statistics.owners")}</span>
         </p>
       </Stats>
     </HeroContainer>
