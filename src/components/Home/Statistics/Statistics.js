e25d95078da96a5410f78d1ad7effcd0ca923965
bigint <bigint@icloud.com>
2022-01-16 02:52:27 +0530

    chore: update stats and add ensnamepicker (#419)

--- src/components/Home/Statistics/Statistics.js
+++ src/components/Home/Statistics/Statistics.js
@@ -91,14 +91,14 @@ export default function Statistics(props) {
       </AnchorContainer>
       <Stats>
         <p>
-          <strong>635k</strong> <span>{t("home.statistics.names")}</span>
+          <strong>637k</strong> <span>{t("home.statistics.names")}</span>
         </p>
         <p>
           <strong>{integrationsCount}</strong>{" "}
           <span>{t("home.statistics.services")}</span>
         </p>
         <p>
-          <strong>271k</strong> <span>{t("home.statistics.owners")}</span>
+          <strong>272k</strong> <span>{t("home.statistics.owners")}</span>
         </p>
       </Stats>
     </HeroContainer>
