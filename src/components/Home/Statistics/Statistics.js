b05294178805020ade618239ee5fa380e8390c1a
bigint <bigint@icloud.com>
2022-02-03 20:53:53 +0530

    chore: update stats and add logos (#434)
    * chore: add glosseta
    
    * chore: add ensbook
    
    * chore: add tempusfinance
    
    * chore: update stats

--- src/components/Home/Statistics/Statistics.js
+++ src/components/Home/Statistics/Statistics.js
@@ -91,14 +91,14 @@ export default function Statistics(props) {
       </AnchorContainer>
       <Stats>
         <p>
-          <strong>672k</strong> <span>{t("home.statistics.names")}</span>
+          <strong>675k</strong> <span>{t("home.statistics.names")}</span>
         </p>
         <p>
           <strong>{integrationsCount}</strong>{" "}
           <span>{t("home.statistics.services")}</span>
         </p>
         <p>
-          <strong>291k</strong> <span>{t("home.statistics.owners")}</span>
+          <strong>292k</strong> <span>{t("home.statistics.owners")}</span>
         </p>
       </Stats>
     </HeroContainer>
