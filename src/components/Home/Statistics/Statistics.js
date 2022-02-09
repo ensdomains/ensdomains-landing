1c3026d2675236b415cfc83e4c481e532359f668
bigint <bigint@icloud.com>
2022-02-09 15:20:14 +0530

    Apply suggestions from code review

--- src/components/Home/Statistics/Statistics.js
+++ src/components/Home/Statistics/Statistics.js
@@ -91,14 +91,14 @@ export default function Statistics(props) {
       </AnchorContainer>
       <Stats>
         <p>
-          <strong>685k</strong> <span>{t("home.statistics.names")}</span>
+          <strong>691k</strong> <span>{t("home.statistics.names")}</span>
         </p>
         <p>
           <strong>{integrationsCount}</strong>{" "}
           <span>{t("home.statistics.services")}</span>
         </p>
         <p>
-          <strong>296k</strong> <span>{t("home.statistics.owners")}</span>
+          <strong>300k</strong> <span>{t("home.statistics.owners")}</span>
         </p>
       </Stats>
     </HeroContainer>
