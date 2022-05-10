e4e4b33112a9f6191eefa594c66763cb4b1f54a7
TateB <yo@taytems.xyz>
2022-05-11 09:33:34 +1000

    fix styles

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -5,13 +5,12 @@ import mq from "../../mediaQuery"
 import { MainPageBannerContainer, DAOBannerContent } from "../DAOBanner"
 
 const Title = styled("h2")`
-  font-size: 24px;
+  font-size: 28px;
   font-weight: 300;
-  margin-top: 100px;
   color: white;
   text-align: center;
   max-width: 800px;
-  padding: 40px 20px 0;
+  padding: 0 20px;
 
   ${mq.medium`
     margin-top: 0;
@@ -24,6 +23,9 @@ const HeroContainer = styled("div")`
   background-image: linear-gradient(90deg, #513eff 0%, #52e5ff 100%);
   display: flex;
   justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 16px;
 `
 
 export default function Hero(props) {
