b475657cda2feb5887ed71dd665a63a0ef1fff08
Serenae.eth <95138671+serenae-fansubs@users.noreply.github.com>
2023-03-01 12:05:50 -0500

    Update Statistics.js

--- src/components/Home/Statistics/Statistics.js
+++ src/components/Home/Statistics/Statistics.js
@@ -91,14 +91,14 @@ export default function Statistics(props) {
       </AnchorContainer>
       <Stats>
         <p>
-          <strong>2.80m</strong> <span>{t("home.statistics.names")}</span>
+          <strong>2.79m</strong> <span>{t("home.statistics.names")}</span>
         </p>
         <p>
           <strong>{integrationsCount}</strong>{" "}
           <span>{t("home.statistics.services")}</span>
         </p>
         <p>
-          <strong>649k</strong> <span>{t("home.statistics.owners")}</span>
+          <strong>662k</strong> <span>{t("home.statistics.owners")}</span>
         </p>
       </Stats>
     </HeroContainer>
