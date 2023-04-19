b41bbf6a0fe96cf427695d4350b48846a834d2f6
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-04-19 14:48:12 +0100

    Remove GA v4

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,19 +52,5 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-101611202-2",
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: [],
-      },
-    },
   ],
 }
