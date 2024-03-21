c53471b5dfc41183783320110eff35885a9e117c
Lucemans <luc@lucemans.nl>
2024-03-21 12:53:15 +0000

    Bump

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+export const nextConfig = {
+    /* */
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'nl'],
+    },
+};
+
+export default nextConfig;
