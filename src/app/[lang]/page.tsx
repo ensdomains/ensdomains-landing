c2ef81168695ce9e2eb4f2fc19265fcfbbebc449
Lucemans <luc@lucemans.nl>
2024-04-03 16:53:33 +0300

    Update Homepage

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,7 +3,6 @@
 import Footer from '../../components/Footer/Footer';
 import { Header } from '../../components/Header/Header';
 import { LanguageSwitcher } from '../../components/LanguageSwitcher/LanguageSwitcher';
-import { LinkList } from '../../components/LinkList/LinkList';
 import { Navbar } from '../../components/Navbar/Navbar';
 import { Language } from '../../i18n/settings';
 import { useTranslation } from '../../i18n/useTranslation';
@@ -25,26 +24,26 @@ export default async function Home({
                 <LanguageSwitcher />
             </div>
 
-            <LinkList
-                links={[
-                    {
-                        title: 'Discord',
-                        href: '',
-                        description: 'Join the community on Discord.',
-                    },
-                    {
-                        title: 'DAO Documentation',
-                        href: '',
-                        description: 'Documentation for the DAO.',
-                    },
-                    {
-                        title: 'Tally Voting',
-                        href: '',
-                        description:
-                            'This repository implements a generic CCIP-Read gateway for fetching state proofs of data on other EVM chains. The intended use is for contracts on L1 to be able to fetch and verify data from contracts on L2 in a read context.',
-                    },
-                ]}
-            />
+            <div>yourname.eth</div>
+
+            <div>your web3 username</div>
+
+            <div>farewell to complexity</div>
+
+            <div>consistent accross the web</div>
+
+            <div>true ownership</div>
+
+            <div>go on your way</div>
+
+            <div>your gateway to the new internet</div>
+
+            <div>
+                <div>a public good shaped by you</div>
+                <div>join our community</div>
+            </div>
+
+            <div>stats (names, integrations, owners)</div>
 
             <Footer />
         </div>
