5e8579f30d8c0ba4bb3eed2e5b591b6a7ab9791d
v1rtl <hi@v1rtl.site>
2024-04-18 03:20:41 +0300

    add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,81 @@
-import 'normalize.css';
 import '../styles/global.css';
 
+import { clsx } from 'clsx';
+import localFont from 'next/font/local';
 import { ReactNode } from 'react';
+
+const ABCMonumentGrotesk = localFont({
+    src: '../../fonts/variable.woff2',
+    variable: '--ens-sans',
+});
+
+const ABCMonumentGroteskMono = localFont({
+    src: [
+        {
+            weight: '400',
+            path: '../../fonts/mono/regular.woff2',
+            style: 'normal',
+        },
+        {
+            weight: '400',
+            path: '../../fonts/mono/italic.woff2',
+            style: 'italic',
+        },
+        {
+            weight: '500',
+            path: '../../fonts/mono/medium.woff2',
+            style: 'normal',
+        },
+        {
+            weight: '500',
+            path: '../../fonts/mono/medium-italic.woff2',
+            style: 'italic',
+        },
+    ],
+    variable: '--ens-mono',
+});
+
+const ABCMonumentGroteskSemiMono = localFont({
+    src: [
+        {
+            weight: '400',
+            path: '../../fonts/semi-mono/regular.woff2',
+            style: 'normal',
+        },
+        {
+            weight: '400',
+            path: '../../fonts/semi-mono/italic.woff2',
+            style: 'italic',
+        },
+        {
+            weight: '500',
+            path: '../../fonts/semi-mono/medium.woff2',
+            style: 'normal',
+        },
+        {
+            weight: '500',
+            path: '../../fonts/semi-mono/medium-italic.woff2',
+            style: 'italic',
+        },
+    ],
+    variable: '--ens-semi-mono',
+});
+
+const ABCMarist = localFont({
+    src: [
+        {
+            weight: '400',
+            path: '../../fonts/serif/regular.woff2',
+            style: 'normal',
+        },
+        {
+            weight: '400',
+            path: '../../fonts/serif/italic.woff2',
+            style: 'italic',
+        },
+    ],
+    variable: '--ens-serif',
+});
 
 export const metadata = {
     title: 'Next.js',
@@ -10,7 +84,15 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
-        <html lang="en">
+        <html
+            lang="en"
+            className={clsx(
+                ABCMonumentGrotesk.variable,
+                ABCMonumentGroteskMono.variable,
+                ABCMonumentGroteskSemiMono.variable,
+                ABCMarist.variable
+            )}
+        >
             <body>{children}</body>
         </html>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,24 @@ export default async function Home() {
 
     return (
         <div>
-            <div>hi</div>
-            <div>plz pick a language</div>
+            <h1
+                style={{
+                    fontFamily: 'var(--ens-sans)',
+                    color: 'var(--ens-green)',
+                    fontSize: 124,
+                }}
+            >
+                title works
+            </h1>
+            <p style={{ fontFamily: 'var(--ens-mono)', fontSize: 36 }}>
+                yeah monospace works too
+            </p>
+            <p style={{ fontFamily: 'var(--ens-semi-mono)', fontSize: 36 }}>
+                semi mono too
+            </p>
+            <h2 style={{ fontFamily: 'var(--ens-serif)', fontSize: 24 }}>
+                serif as well
+            </h2>
             <LanguageSwitcher />
         </div>
     );
