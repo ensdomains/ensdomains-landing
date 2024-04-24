f6f9442f7a607aef5010ade15b0dc4ded862765f
v1rtl <hi@v1rtl.site>
2024-04-25 01:15:14 +0300

    animate profile when in viewport

--- src/components/DappsAnimation/DappsAnimation.tsx
+++ src/components/DappsAnimation/DappsAnimation.tsx
@@ -1,11 +1,34 @@
 import { clsx } from 'clsx';
+import { useRef, useState } from 'react';
 
 import ui from '../../styles/ui.module.css';
+import { useEventListener } from '../../utils/useEventListener';
+import { useIntersectionObserver } from '../../utils/useIntersectionObserver';
 import styles from './DappsAnimation.module.css';
 
 export const DappsAnimation = () => {
+    const { ref, isIntersecting } = useIntersectionObserver({
+        threshold: 1,
+    });
+
+    const fndProfile = useRef<HTMLDivElement>();
+    const [isAnimating, setIsAnimating] = useState(true);
+
+    useEventListener(
+        'transitionend',
+        () => {
+            if (isIntersecting) {
+                setTimeout(() => {
+                    setIsAnimating(false);
+                }, 1000);
+            }
+        },
+        fndProfile
+    );
+
     return (
         <div
+            ref={ref}
             className={clsx(
                 styles.grid,
                 ui.flex,
@@ -23,7 +46,14 @@ export const DappsAnimation = () => {
             >
                 <div className={styles.fndBg}></div>
                 <div className={styles.fndPfp}></div>
-                <div className={styles.fndName}>placeholder.eth</div>
+                <div
+                    className={clsx(
+                        styles.fndName,
+                        isIntersecting && isAnimating && styles.animating
+                    )}
+                >
+                    placeholder.eth
+                </div>
                 <div className={styles.fndLine}></div>
                 <div
                     className={clsx(ui.flex, ui['flex-row'], styles.fndGallery)}
@@ -46,6 +76,15 @@ export const DappsAnimation = () => {
                         height={74}
                         alt=""
                     />
+                </div>
+            </div>
+            <div className={styles.swap}>
+                <div className={styles.header}>
+                    <span>Swap</span>
+                    <div>
+                        <div></div>
+                        <div>placeholder.eth</div>
+                    </div>
                 </div>
             </div>
         </div>
