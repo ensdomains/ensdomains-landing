83597141f64475843082986a29c198a79e025d29
v1rtl <hi@v1rtl.site>
2024-05-13 23:13:43 +0300

    static elements for developers page

--- src/components/developers/header/HeroContent.tsx
+++ src/components/developers/header/HeroContent.tsx
@@ -1,8 +1,16 @@
-import styles from './Hero.module.css';
+import styles from './HeroContent.module.css';
 
 export const HeroContent = () => {
     return (
-        <div className={styles['position-container']}>
-        </div>
+        <>
+            <div className={styles['position-container']}>
+                <img src="/assets/developers/dark-magenta-element.svg" height={400} width={400} alt="" />
+
+            </div>
+            <div className={styles['position-container']}>
+                <img src="/assets/developers/magenta-element.svg" height={390} width={284} alt="" />
+            </div>
+        </>
+
     );
 };
