05cac193faf0fd9f7ad8866ffd4b9eb524c5c205
v1rtl <hi@v1rtl.site>
2024-06-22 22:40:14 +0300

    governance header animation wip

--- src/components/governance/HeroContent.tsx
+++ src/components/governance/HeroContent.tsx
@@ -1,18 +1,38 @@
 import { AnimatedSquare } from '~/components/AnimatedSquare/AnimatedSquare';
 import styles from './HeroContent.module.css';
+import { ResponsiveImage } from '../ResponsiveImage/ResponsiveImage';
 
 export const HeroContent = () => {
     return (
         <>
-            <div className={styles['position-container']}>
+            <div className={styles['position-container']} data-name="turn-left">
                 <div className={styles['animation-container']}>
-                    <img src="/assets/governance/left-desktop.svg" height={922} width={354} alt="" />
+                    <ResponsiveImage
+                        sources={{
+                            desktop: '/assets/governance/diagonal-left.svg',
+                            mobile: '/assets/governance/diagonal-left-tablet.svg',
+                        }}
+                        sourceProps={{
+                            desktop: { height: 200, width: 200 },
+                            mobile: { height: 135, width: 135 },
+                        }}
+                    />
                     <AnimatedSquare className={styles['animated-square']} />
                 </div>
+
             </div>
-            <div className={styles['position-container']}>
+            <div className={styles['position-container']} data-name="turn-right">
                 <div className={styles['animation-container']}>
-                    <img src="/assets/governance/right-desktop.svg" height={673} width={354} alt="" />
+                    <ResponsiveImage
+                        sources={{
+                            desktop: '/assets/governance/diagonal-right.svg',
+                            mobile: '/assets/governance/diagonal-right-tablet.svg',
+                        }}
+                        sourceProps={{
+                            desktop: { height: 200, width: 200 },
+                            mobile: { height: 135, width: 135 },
+                        }}
+                    />
                     <AnimatedSquare className={styles['animated-square']} />
                 </div>
             </div>
