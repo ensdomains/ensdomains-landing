523be56280882eef055bf9934c8c675843a330be
v1rtl <hi@v1rtl.site>
2024-07-08 00:26:44 +0300

    fix button

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -218,7 +218,7 @@ export default async function Home({ params: { lang = fallbackLng } }: { params:
                             ),
                             button: t('home.extra.community.button'),
                             buttonVariant: 'secondary',
-                            href: '/',
+                            href: 'https://discuss.ens.domains',
                         },
                     ]}
                 />
