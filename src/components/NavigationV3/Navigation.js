8347cc238468a5be35efafaf7dc3f8a344cd4442
storywithoutend <storywithoutend@gmail.com>
2024-07-08 11:01:11 +0800

    update home navigation link

--- src/components/NavigationV3/Navigation.js
+++ src/components/NavigationV3/Navigation.js
@@ -1,60 +1,62 @@
-import React from "react"
-import styled, { css } from "styled-components"
-import { Link } from "gatsby"
-import Logo from "../Logov2"
-import { Typography, mq } from "@ensdomains/thorin_next"
+import { mq, Typography } from '@ensdomains/thorin_next';
+import { Link } from 'gatsby';
+import React from 'react';
+import styled, { css } from 'styled-components';
 
-import Hamburger from "./Hamburger"
+import Logo from '../Logov2';
+import Hamburger from './Hamburger';
 
 const Nav = styled.nav(({ theme }) => [
-  css`
-    position: sticky;
-    display: flex;
-    justify-content: space-between;
-    max-width: 940px;
-    margin: 0 auto;
-    padding: ${theme.space["6"]} ${theme.space["4"]};
-  `,
-  mq.md.min(css`
-    padding: ${theme.space["10"]};
-  `),
-])
+    css`
+        position: sticky;
+        display: flex;
+        justify-content: space-between;
+        max-width: 940px;
+        margin: 0 auto;
+        padding: ${theme.space['6']} ${theme.space['4']};
+    `,
+    mq.md.min(css`
+        padding: ${theme.space['10']};
+    `),
+]);
 
 const NavRight = styled.div(
-  ({ theme }) => css`
-    display: flex;
-    align-items: center;
-    gap: ${theme.space[5]};
-  `
-)
+    ({ theme }) => css`
+        display: flex;
+        align-items: center;
+        gap: ${theme.space[5]};
+    `
+);
 
-const StyledLink = styled(Link)(({ theme }) => css`
-  color: ${theme.colors.greyPrimary};
-  transition: color 0.2s;
-  &:hover {
-    color: ${theme.colors.accentPrimary};
-  }
-`)
+const StyledLink = styled(Link)(
+    ({ theme }) => css`
+        color: ${theme.colors.greyPrimary};
+        transition: color 0.2s;
+        &:hover {
+            color: ${theme.colors.accentPrimary};
+        }
+    `
+);
 
 export default function Navigation() {
-  return (
-    <Nav>
-      <Link to="/">
-        <Logo style="blue"/>
-      </Link>
-      <NavRight>
-        <StyledLink to="/">
-          <Typography fontVariant="bodyBold" color="inherit">
-            ENS Domains
-          </Typography>
-        </StyledLink>
-        <StyledLink to="/l2-roadmap">
-          <Typography fontVariant="bodyBold" color="inherit">
-            L2 Roadmap
-          </Typography>
-        </StyledLink>
-        <Hamburger />
-      </NavRight>
-    </Nav>
-  )
+    return (
+        <Nav>
+            <Link to="https://ens.domains">
+                <Logo style="blue" />
+            </Link>
+            <NavRight>
+                <StyledLink to="/">
+                    <Typography fontVariant="bodyBold" color="inherit">
+                        ENS Domains
+                    </Typography>
+                </StyledLink>
+                <StyledLink to="/l2-roadmap">
+                    <Typography fontVariant="bodyBold" color="inherit">
+                        L2 Roadmap
+                    </Typography>
+                </StyledLink>
+                <Hamburger />
+            </NavRight>
+        </Nav>
+    );
 }
