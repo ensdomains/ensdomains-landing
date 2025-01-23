1c3a38143f393ef54112662346bfb4cbc0a63e5b
Jakob <jakob.helgesson@gmail.com>
2025-01-23 18:17:06 +0100

    Add Blog components and styles for header, author, and post previews

---
+++ src/components/Blog/Post/PostFooter.tsx
@@ -0,0 +1,49 @@
+import { BlogPostMetadata } from '~/utils/blog/metadata'
+import styles from './PostFooter.module.css'
+import clsx from 'clsx'
+import Link from 'next/link'
+import { getAuthorAssets } from '~/utils/blog/utils'
+import Image from 'next/image'
+
+export const FooterAuthor = async ({ author }: { author: string }) => {
+  const authorData = getAuthorAssets(author)
+  const avatar = await authorData?.avatar
+
+  return (
+    <div className={styles.author}>
+      <div className={styles.authorTitle}>Author</div>
+      <div className={styles.authorData}>
+        <div>
+          <div className={styles.authorName}>{author}</div>
+          <Link
+            href={`/blog/author/${author}`}
+            className={clsx(styles.authorLink)}
+          >
+            View articles
+          </Link>
+        </div>
+        {authorData?.records.description && (
+          <div className={clsx(styles.authorDescription, styles.authorDesktop)}>
+            {authorData.records.description}
+          </div>
+        )}
+        {avatar ? <Image src={avatar} alt={author} className={styles.authorAvatar} /> : <div className={styles.authorAvatar} />}
+      </div>
+      {authorData?.records.description && (
+        <div className={clsx(styles.authorDescription, styles.authorMobile)}>
+          {authorData.records.description}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export const PostFooter = ({ post }: { post: BlogPostMetadata }) => {
+  return (
+    <div className={clsx(styles.footer)}>
+      {post.authors?.map(author => (
+        <FooterAuthor key={author} author={author} />
+      ))}
+    </div>
+  )
+}
---
+++ src/components/Blog/PostPreview.tsx
@@ -0,0 +1,65 @@
+import clsx from 'clsx'
+import { ABCMarist } from 'fonts'
+import Link from 'next/link'
+import { BlogPostMetadataPlus } from '~/utils/blog/posts'
+import Image from 'next/image'
+import styles from './PostPreview.module.css'
+import { BlogPostAuthor } from './PostAuthor'
+import { getAuthorAssets, getPostAssets } from '~/utils/blog/utils'
+
+export const BlogPostPreview = async ({ post, highlighted = false }: { post: BlogPostMetadataPlus, highlighted?: boolean }) => {
+  const cover = await getPostAssets(post.file)?.['cover-thumb']
+
+  const date = new Date(post.date).toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  })
+
+  return (
+    <Link
+      href={'/blog/post/' + post.slug}
+      className={clsx(styles['preview-link'], highlighted && styles['preview-link-highlighted'])}
+    >
+      <div className={styles['preview-cover-container']}>
+        {cover
+          ? (
+              <Image
+                src={cover}
+                className={styles['preview-cover']}
+                alt={post.title}
+              />
+            )
+          : (
+              <img
+                src={post.cover}
+                className={styles['preview-cover']}
+                alt={post.title}
+              />
+            )}
+      </div>
+      {/* Blog post title, max two lines then ellipsis */}
+      <div className={clsx(styles['preview-content-container'], highlighted && styles['preview-content-container-highlighted'])}>
+        <span className={styles['preview-title']}>
+          {post.title}
+        </span>
+
+        <span
+          className={clsx(ABCMarist.className, styles['preview-description'])}
+        >
+          {post.description}
+        </span>
+
+        <span className={clsx(styles['preview-authors'])}>
+          {post.authors.map(async author => (
+            <BlogPostAuthor key={author} author={author} avatar={await getAuthorAssets(author)?.avatar} />
+          ))}
+          {/* Date */}
+          <span>
+            {date}
+          </span>
+        </span>
+      </div>
+    </Link>
+  )
+}
