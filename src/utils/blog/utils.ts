7281d9b9ccaa8a8ef639efd4c8d3e5fa8ca99202
Jakob <jakob.helgesson@gmail.com>
2025-01-23 18:08:37 +0100

    Implement blog post metadata management and enhance search functionality
    - Added new utilities for managing blog post metadata, including `metadata.ts` and `posts.ts`, to handle post data and metadata validation using Zod.
    - Introduced a JSON schema for blog post metadata in `schema.json` to enforce structure and validation.
    - Implemented search functionality in `search.ts` to fetch and process search results from an external API.
    - Updated `utils.ts` to define the posts directory and asset retrieval functions.
    - Enhanced package dependencies in `package.json` to support new features.
    
    This update improves the blog's content management and search capabilities, providing a more structured approach to handling blog posts.

---
+++ src/utils/blog/utils.ts
@@ -0,0 +1,19 @@
+import { type Author, authors, type Post, posts } from 'build-assets/assets.gen'
+
+const CURRENT_DIR = new URL(import.meta.url)
+
+export const POSTS_DIR = new URL('../../../posts/', CURRENT_DIR)
+
+export const getPostAssets = (slug: string) => {
+  return posts[slug] as Post | undefined
+}
+
+export const getAuthorAssets = (name: string) => {
+  return authors[name] as Author | undefined
+}
+
+export class AssetNotFoundError extends Error {
+  constructor(asset: string) {
+    super(`Asset ${asset} not found. Try doing pnpm build:assets`)
+  }
+}
